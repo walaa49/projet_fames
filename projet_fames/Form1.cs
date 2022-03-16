42cdda696439f806d62e9f6b31064180036bbbf6
messamine <amine.dev28@gmail.com>
2022-03-16 15:51:59 +0100

    Revert "login form"
    This reverts commit 04890c3acb42e58368a02f9484dead8d85641880.

--- projet_fames/Form1.cs
+++ projet_fames/Form1.cs
@@ -20,7 +20,6 @@ namespace projet_fames
         private void Form1_Load(object sender, EventArgs e)
         {
             btnconnection.Hide();
-            pnlcreation.Hide();
         }
 
         private void btnconnection_Click(object sender, EventArgs e)
@@ -28,11 +27,6 @@ namespace projet_fames
             timer2.Start();
             btnconnection.Hide();
             btncreation.Show();
-           
-           
-            pnlcreation.Show();
-            pnlgauche.Show();
-            pnlcreation.Hide();
         }
 
         private void btncreation_Click(object sender, EventArgs e)
@@ -40,10 +34,6 @@ namespace projet_fames
             timer1.Start();
             btncreation.Hide();
             btnconnection.Show();
-            pnlgauche.Show();
-           
-            pnlcreation.Show();
-            pnlgauche.Hide();
         }
 
         private void timer1_Tick(object sender, EventArgs e)
