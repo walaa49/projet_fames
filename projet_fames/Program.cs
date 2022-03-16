83bf6b4a4941fd49e35cef9d54b3eeb5ef6f9929
messamine <amine.dev28@gmail.com>
2022-03-16 15:58:55 +0100

    modif

--- projet_fames/Program.cs
+++ projet_fames/Program.cs
@@ -16,7 +16,7 @@ namespace projet_fames
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new dash());
+            Application.Run(new Form1());
         }
     }
 }
